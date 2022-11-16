8a84023086eff73e323542b83dd8f8fce6e82ad2
TrumanYen <yen.truman@gmail.com>
2022-11-16 12:03:51 -0700

    Added Truman old code

---
+++ TrumanOldCode/Workout.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Workout {
+    //dummy class for dashboard
+    private int workoutNum;
+    public Workout(int num){
+        workoutNum = num;
+    }
+    public int getNum(){
+        return workoutNum;
+    }
+}
