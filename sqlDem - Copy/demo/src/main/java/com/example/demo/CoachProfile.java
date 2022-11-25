a992073e719687d1072f85612c89756daa15c136
williamchan2 <87112587+williamchan2@users.noreply.github.com>
2022-11-25 15:44:46 -0700

    the beginning of coach/athlete

---
+++ sqlDem - Copy/demo/src/main/java/com/example/demo/CoachProfile.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class CoachProfile extends ProfileScreen {
+
+    public CoachProfile(int userID){
+        super(userID);
+    }
+    
+        // select athlete_id from accesses_information where coach_id = ?;
+
+}
