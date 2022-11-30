53234d3450a3468d84401a30b008b79269587ad7
TrumanYen <yen.truman@gmail.com>
2022-11-30 15:27:35 -0700

    Revert "?"
    This reverts commit 4624cede8dfcf0083ba736ade72bcd269f30255a.

--- sqlDem - Copy/demo/src/main/java/com/example/demo/MealView.java
+++ sqlDem - Copy/demo/src/main/java/com/example/demo/MealView.java
@@ -2,29 +2,20 @@ package com.example.demo;
 import java.sql.*;
 import java.rmi.server.RemoteObject;
 import java.util.ArrayList;
-
-import org.apache.logging.log4j.util.MultiFormatStringBuilderFormattable;
-
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.component.accordion.Accordion;
-import com.vaadin.flow.component.accordion.AccordionPanel;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-public class MealView extends VerticalLayout implements Editor<Meal>{
+public class MealView implements Editor<Meal>{
     private ArrayList<Food> foodList = new ArrayList<Food>();
     private int userID;
     private Connection con;
-    private ArrayList<Meal> mealList;
-    private Accordion mealAccordion;
     private Button newMeal = new Button("New Meal");
     private Button editMeal = new Button("Edit Meal");
     private Button delMeal = new Button("Delete Meal");
-    private Paragraph noItems = new Paragraph("No meals on this day");
-    private ArrayList<AccordionPanel> panelArray = new ArrayList<AccordionPanel>();
+    ComboBox<Food> chooseFood = new ComboBox<Food>("Choose Meal");
     public MealView(int ID)
     {
         userID = ID;
@@ -60,19 +51,6 @@ public class MealView extends VerticalLayout implements Editor<Meal>{
 
         */
     }
-    private void initAccordion(){
-        //build accordion
-        mealAccordion = new Accordion();
-        for (Meal m: mealList){
-            AccordionPanel tmp = new AccordionPanel("Workout "+mealList.indexOf(m) +":",  new MealEditor(this, m));
-            panelArray.add(tmp);
-            mealAccordion.add(tmp);
-        }
-        add(mealAccordion);
-        if (mealList.isEmpty()){
-            add(noItems);
-        }
-    }
     public void initConnection()
     {
          
@@ -90,7 +68,22 @@ public class MealView extends VerticalLayout implements Editor<Meal>{
     @Override
     public void fetchData()
     {
-       
+        try
+        {
+            PreparedStatement query = con.prepareStatement("SELECT Name FROM FOOD WHERE User_ID = ?;");
+            query.setInt(1, userID);
+            ResultSet rs = query.executeQuery();
+            foodList = new ArrayList<Food>();
+            while(rs.next())
+            {
+                foodList.add(new Food(userID,rs.getString(1)));
+            }
+            chooseFood.setItems(foodList);
+        }
+        catch(SQLException e)
+        {
+
+        }
     }
     @Override
     public void deleteObject(Meal meal)
